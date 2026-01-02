7c87e368f2a38199771c1a22a5077992002f0da1
Danzelita <danzloff@gmail.com>
2026-01-03 01:50:02 +0400

    Week11_1

---
+++ Server/src/Library.ts
@@ -0,0 +1,7 @@
+export namespace Library{
+    
+    export const phpKEY = '42!3df-b!@1qvK';
+
+    export const getDeckURL = 'http://localhost/Projects/ClashRoyaleServer/Multiplayer/getDeck.php';
+
+}
